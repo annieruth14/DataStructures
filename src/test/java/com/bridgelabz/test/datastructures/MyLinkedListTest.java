4ff49ec579c40349008075614bf1c70ef8f5c023
annieruth14 <ruthannie3@gmail.com>
2020-10-11 00:51:25 +0530

    [Annie]Added: pop method to delete the first element in a linked list

--- src/test/java/com/bridgelabz/test/datastructures/MyLinkedListTest.java
+++ src/test/java/com/bridgelabz/test/datastructures/MyLinkedListTest.java
@@ -18,6 +18,7 @@ public class MyLinkedListTest {
 				&& myLinkedList.tail.equals(myFirstNode);
 		Assert.assertTrue(result);
 	}
+
 	@Test
 	public void givenNumbers_WhenAppendToLinkedList_shouldBeAppendedToLast() {
 		MyNode<Integer> myFirstNode = new MyNode<>(70);
@@ -32,7 +33,7 @@ public class MyLinkedListTest {
 				&& myLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
-	
+
 	@Test
 	public void givenNumbers_WhenInsertInLinkedList_shouldBeInserted() {
 		MyNode<Integer> myFirstNode = new MyNode<>(56);
@@ -46,5 +47,21 @@ public class MyLinkedListTest {
 		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
 				&& myLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void givenNumbers_WhenPoppedFromALinkedList_shouldReturnHead() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.append(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.printMyNodes();
+		INode popped = myLinkedList.pop();
+		System.out.println("After popping : ");
+		myLinkedList.printMyNodes();
+		Assert.assertEquals(56, popped.getKey());
 	}
 }
