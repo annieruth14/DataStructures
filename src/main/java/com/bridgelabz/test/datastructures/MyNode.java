8ed29ed995f2b7fb69f1c597b681362fefbd1292
annieruth14 <ruthannie3@gmail.com>
2020-10-10 12:30:36 +0530

    [Annie]Added: Welcome message

---
+++ src/main/java/com/bridgelabz/test/datastructures/MyNode.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.test.datastructures;
+
+public class MyNode {
+
+	public void welcome() {
+		System.out.println("Welcome to problems on Data Structures");
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/test/datastructures/MyNodeTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.test.datastructures;
+
+import org.junit.Test;
+
+public class MyNodeTest {
+	MyNode myNode = new MyNode();
+
+	@Test
+	public void start() {
+		myNode.welcome();
+	}
+}
