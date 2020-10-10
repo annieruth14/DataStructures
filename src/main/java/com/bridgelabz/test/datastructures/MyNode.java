a7c42df5f23f34741fbb3c0f81466996892afe33
annieruth14 <ruthannie3@gmail.com>
2020-10-10 22:48:36 +0530

    [Annie]Added : method and test case to create linked list and add keys

--- src/main/java/com/bridgelabz/test/datastructures/MyNode.java
+++ src/main/java/com/bridgelabz/test/datastructures/MyNode.java
@@ -1,9 +1,28 @@
 package com.bridgelabz.test.datastructures;
 
-public class MyNode {
+public class MyNode<T> {
+	private T key;
+	private MyNode next;
 
-	public void welcome() {
-		System.out.println("Welcome to problems on Data Structures");
+	public MyNode(T key) {
+		super();
+		this.key = key;
+		// this.next = next;
 	}
 
+	public T getKey() {
+		return key;
+	}
+
+	public void setKey(T key) {
+		this.key = key;
+	}
+
+	public MyNode getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
 }
--- src/test/java/com/bridgelabz/test/datastructures/MyNodeTest.java
+++ src/test/java/com/bridgelabz/test/datastructures/MyNodeTest.java
@@ -1,12 +1,20 @@
 package com.bridgelabz.test.datastructures;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MyNodeTest {
-	MyNode myNode = new MyNode();
+	MyNode myNode = null;
 
 	@Test
-	public void start() {
-		myNode.welcome();
+	public void givenNumbers_whenCreateLinkedList_ShouldReturnTrue() {
+		MyNode<Integer> myFirst = new MyNode<>(56);
+		MyNode<Integer> mySecond = new MyNode<>(30);
+		MyNode<Integer> myThird = new MyNode<>(70);
+		myFirst.setNext(mySecond);
+		mySecond.setNext(myThird);
+		boolean result = myFirst.getNext().equals(mySecond) && myFirst.getNext().getNext().equals(myThird);
+		Assert.assertTrue(result);
 	}
+
 }
