1e9261c119bf85b33d848809dc2a0bebaaa23f12
annieruth14 <ruthannie3@gmail.com>
2020-10-11 21:38:37 +0530

    [Annie]Added :method to pop element from stack and return peak

--- src/main/java/com/bridgelabz/test/datastructures/MyStack.java
+++ src/main/java/com/bridgelabz/test/datastructures/MyStack.java
@@ -6,12 +6,20 @@ public class MyStack {
 	public MyStack() {
 		this.myLinkedList = new MyLinkedList();
 	}
+
 	public void push(INode myNode) {
 		myLinkedList.add(myNode);
 	}
-	
+
 	public void printStack() {
 		myLinkedList.printMyNodes();
 	}
 
+	public INode peak() {
+		return myLinkedList.head;
+	}
+
+	public INode pop() {
+		return myLinkedList.pop();
+	}
 }
--- src/test/java/com/bridgelabz/test/datastructures/MyStackTest.java
+++ src/test/java/com/bridgelabz/test/datastructures/MyStackTest.java
@@ -13,6 +13,22 @@ public class MyStackTest {
 		myStack.push(myFirstNode);
 		myStack.push(mySecondNode);
 		myStack.push(myThirdNode);
+		INode myNode = myStack.peak();
 		myStack.printStack();
+		Assert.assertEquals(myThirdNode, myNode);
+	}
+	@Test
+	public void given3Numbers_whenPoppedStack_shouldMatchWithLastAddedNode() {
+		MyStack myStack = new MyStack();
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		myStack.push(myFirstNode);
+		myStack.push(mySecondNode);
+		myStack.push(myThirdNode);
+		INode myNode1 = myStack.pop();
+		INode myNode2 = myStack.pop();
+		INode myNode3 = myStack.pop();
+		Assert.assertEquals(myNode3.getKey() , 70);
 	}
 }
