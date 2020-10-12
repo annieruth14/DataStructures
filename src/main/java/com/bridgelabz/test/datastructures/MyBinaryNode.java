f314da79a73a77cf2e92783125a4d08a8c3c190a
annieruth14 <ruthannie3@gmail.com>
2020-10-12 22:07:36 +0530

    [Annie]Added: nodes in BST

--- src/main/java/com/bridgelabz/test/datastructures/MyBinaryNode.java
+++ src/main/java/com/bridgelabz/test/datastructures/MyBinaryNode.java
@@ -4,11 +4,12 @@ public class MyBinaryNode<K extends Comparable<K>> {
 	K key;
 	MyBinaryNode<K> left;
 	MyBinaryNode<K> right;
+
 	public MyBinaryNode(K key) {
 		super();
 		this.key = key;
 		this.left = null;
 		this.right = null;
 	}
-	
+
 }
--- src/main/java/com/bridgelabz/test/datastructures/MyBinaryTree.java
+++ src/main/java/com/bridgelabz/test/datastructures/MyBinaryTree.java
@@ -1,22 +1,21 @@
 package com.bridgelabz.test.datastructures;
 
-public class MyBinaryTree <K extends Comparable<K>> {
+public class MyBinaryTree<K extends Comparable<K>> {
 	private MyBinaryNode<K> root;
-	
+
 	public void add(K key) {
 		this.root = this.addRecursively(root, key);
 	}
 
 	public MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
-		if(current == null)
+		if (current == null)
 			return new MyBinaryNode(key);
 		int compareResult = key.compareTo(current.key);
-		if(compareResult == 0)
+		if (compareResult == 0)
 			return current;
-		if(compareResult<0) {
+		if (compareResult < 0) {
 			current.left = addRecursively(current.left, key);
-		}
-		else {
+		} else {
 			current.right = addRecursively(current.right, key);
 		}
 		return current;
@@ -27,6 +26,6 @@ public class MyBinaryTree <K extends Comparable<K>> {
 	}
 
 	public int getSizeRecursive(MyBinaryNode<K> current) {
-		return current == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right); 
+		return current == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
 	}
 }
