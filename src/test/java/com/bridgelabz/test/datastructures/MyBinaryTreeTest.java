94a9626196c5ad933845c105104b75e2bee33528
annieruth14 <ruthannie3@gmail.com>
2020-10-13 09:04:15 +0530

    [Annie]Refactored : methods to avoid repitition of same code

--- src/test/java/com/bridgelabz/test/datastructures/MyBinaryTreeTest.java
+++ src/test/java/com/bridgelabz/test/datastructures/MyBinaryTreeTest.java
@@ -1,22 +1,14 @@
 package com.bridgelabz.test.datastructures;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyBinaryTreeTest<K> {
-	@Test
-	public void given3Numbers_whenAddedToBinaryTree_shouldReturnTree() {
-		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
-		myBinaryTree.add(56);
-		myBinaryTree.add(30);
-		myBinaryTree.add(70);
-		int size = myBinaryTree.getSize();
-		Assert.assertEquals(3, size);
-	}
-
-	@Test
-	public void givenNumbers_whenAddedToBinaryTree_shouldReturnTree() {
-		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+	MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+	
+	@Before
+	public void initializeTree() {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
@@ -31,28 +23,26 @@ public class MyBinaryTreeTest<K> {
 		myBinaryTree.add(67);
 		myBinaryTree.add(16);
 		myBinaryTree.add(63);
+	}
+	@Test
+	public void given3Numbers_whenAddedToBinaryTree_shouldReturnTree() {
+		MyBinaryTree<Integer> myBinaryTree1 = new MyBinaryTree<>();
+		myBinaryTree1.add(56);
+		myBinaryTree1.add(30);
+		myBinaryTree1.add(70);
+		int size = myBinaryTree1.getSize();
+		Assert.assertEquals(3, size);
+	}
+
+	@Test
+	public void givenNumbers_whenAddedToBinaryTree_shouldReturnTree() {
 		int size = myBinaryTree.getSize();
 		Assert.assertEquals(13, size);
 	}
 
 	@Test
 	public void givenNumbers_whenSearchInBinaryTree_shouldReturnNode() {
-		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
-		myBinaryTree.add(56);
-		myBinaryTree.add(30);
-		myBinaryTree.add(70);
-		myBinaryTree.add(22);
-		myBinaryTree.add(40);
-		myBinaryTree.add(60);
-		myBinaryTree.add(95);
-		myBinaryTree.add(30);
-		myBinaryTree.add(65);
-		myBinaryTree.add(11);
-		myBinaryTree.add(3);
-		myBinaryTree.add(67);
-		myBinaryTree.add(16);
-		myBinaryTree.add(63);
-		Integer result = myBinaryTree.search(63);
-		Assert.assertEquals(result, (Integer) 63);
+		K result = (K) myBinaryTree.search(63);
+		Assert.assertEquals(result, 63);
 	}
 }
